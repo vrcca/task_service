385cf4718d31b135bcd5e74b9f7e56bc62c85c47
Vitor Cavalcanti de Albuquerque <1920397+vrcca@users.noreply.github.com>
2019-06-29 11:35:53 -0300

    Returns same order when tasks have no dependencies

--- test/task_service/domain/execution_planner_test.exs
+++ test/task_service/domain/execution_planner_test.exs
@@ -2,15 +2,29 @@ defmodule TaskService.ExecutionPlannerTest do
   use ExUnit.Case
   alias TaskService.ExecutionPlanner
 
-  test "returns one command when there is only one" do
-    tasks = [
-      %{
-        name: "task-1",
-        command: "touch /tmp/file"
-      }
-    ]
+  test "empty tasks returns empty plan" do
+    tasks = []
+    plan = ExecutionPlanner.create(tasks)
+    assert [] == plan
+  end
+
+  test "returns one command when there is only a task" do
+    tasks = [a_task("tmp")]
 
     plan = ExecutionPlanner.create(tasks)
     assert ^tasks = plan
+  end
+
+  test "returns same order when tasks does not contain dependencies" do
+    tasks = [a_task("tmp"), a_task("rm")]
+    plan = ExecutionPlanner.create(tasks)
+    assert ^tasks = plan
+  end
+
+  defp a_task(name) do
+    %{
+      name: name,
+      command: "touch /tmp/#{name}"
+    }
   end
 end
